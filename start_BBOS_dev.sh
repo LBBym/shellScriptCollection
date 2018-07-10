7b6645101d7198fcd62f1f4304e267919f26e70f
LBB <liubbpro@gmail.com>
2018-07-10 23:04:06 +0800

    BBOS and readme

---
+++ start_BBOS_dev.sh
@@ -0,0 +1,6 @@
+code
+
+#. ./start_BBOS_dev.sh
+cd /home/lbb/CS/BBOS
+
+google-chrome 'https://github.com/LBBym' 'https://github.com/s-matyukevich/raspberry-pi-os' 'https://github.com/s-matyukevich/raspberry-pi-os/tree/master/src'
